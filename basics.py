ec03683098455259c9cab51ee2ba2c894766de69
jensen yuen <jensenyuen@outlook.com>
2022-11-05 17:13:36 +0900

    added files

---
+++ basics.py
@@ -0,0 +1,13 @@
+message = "Hello world"
+
+#print (message)
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+strings = str(numbers)
+
+#print(type(numbers[1]))
+#rint(type(strings[1]))
+
+for num in numbers:
+  next
+  #print("This number: %s"%num)
---
+++ mileage_convertor.py
@@ -0,0 +1,7 @@
+# This is a code along
+# This program converts Kilometers to miles
+
+print("How many kilometers did you cycle today?")
+kms = float(input())
+miles = kms/1.60934
+print(f"That is equl to {round(miles,3)} miles")
