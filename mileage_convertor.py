eed504016a3ed74616c0c29453b69ee5fc529663
jensen yuen <jensenyuen@outlook.com>
2022-11-05 17:15:49 +0900

    minor changes

--- mileage_convertor.py
+++ mileage_convertor.py
@@ -4,4 +4,4 @@
 print("How many kilometers did you cycle today?")
 kms = float(input())
 miles = kms/1.60934
-print(f"That is equl to {round(miles,3)} miles")
+print(f"Your {kms}km cycle is equal to {round(miles,2)}mi")
